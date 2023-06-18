5f1247cb26c75aa9747e54fc40c91ddbc91a5036
jaider2023 <jaider321@gmail.com>
2023-06-18 10:00:34 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- *  * main   A program that prints a line using the printf function
- *
- *   * Return: 0 (Success)
- *
- *    */
-
+ *  main   A program that prints a line using the printf functio
+ n 
+ *  Return: 0 (Success)
+ */
 
 
 int main(void)
