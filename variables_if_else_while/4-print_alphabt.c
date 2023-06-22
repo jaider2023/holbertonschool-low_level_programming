c7038147d8b8e78f23d81a0f1f0983f05b7bebc1
jaider2023 <jaider321@gmail.com>
2023-06-22 09:01:09 -0700

    print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,12 @@
 int main(void)
 {
 int a;
-for (a = 97; a <= 123; a++)
+for (a = 97; a <= 122; a++)
 {
-putchar(a);
-}
 if (a != 113 && a != 101)
 {
 putchar(a);
+}
 }
 putchar('\n');
 return (0);
