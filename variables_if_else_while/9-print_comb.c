0917cf4602dfa47f4f73f322d81d89bdafb438a8
jaider2023 <jaider321@gmail.com>
2023-06-22 11:34:12 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+int digit1, digit2, digit3, digit4;
+for (digit1 = 0; digit1 < 10; digit1++)
+{
+for (digit2 = 0; digit2 < 10; digit2++)
+{
+for (digit3 = 0; digit3 < 10; digit3++)
+{
+for (digit4 = 0; digit4 < 10; digit4++)
+{
+putchar('0' + digit1);
+putchar('0' + digit2);
+putchar('0' + digit3);
+putchar('0' + digit4);
+if (digit1 != 9 || digit2 != 9 || digit3 != 9 || digit4 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
