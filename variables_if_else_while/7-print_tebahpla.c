780e243d90354466222177541748a8f3ea0eb022
jaider2023 <jaider321@gmail.com>
2023-06-22 10:58:51 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 122; a >= 97; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
