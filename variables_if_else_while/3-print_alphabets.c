df9ca34561aa480d41b387a001b4210177952d1a
jaider2023 <jaider321@gmail.com>
2023-06-22 08:26:52 -0700

    4-print_alphabt.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
+int a;
+for (a = 97; a <= 123; a++)
+{
+putchar(a);
+}
 int A;
 for (A = 65; A <= 90; A++)
 {
---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 97; a <= 123; a++)
+{
+putchar(a);
+}
+if (a != 'q' && a != 'e')
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
