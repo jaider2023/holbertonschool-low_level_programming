f4b70b3be94c8db0024354ae2bd5a0560bd25ea2
jaider2023 <jaider321@gmail.com>
2023-06-23 12:41:43 -0700

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 int _putchar(char c)
 {
 return putchar(c);
 }
-int main(void) {
+int main(void)
+{
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -14,5 +14,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return 0;
+return (0);
 }
