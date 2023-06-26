394a02763511df61cd7c6f8eafc80935fb80d237
jaider2023 <jaider321@gmail.com>
2023-06-25 20:08:07 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/_putchar.c
+++ more_functions_nested_loops/_putchar.c
@@ -1,7 +1,8 @@
-#include <unistd.h>
+#include <stdio.h>
+
+
 
 int _putchar(char c)
 {
-
-return write(1, &c, 1);
+return putchar(c);
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
 int _isdigit(int x);
-int mul(int x, int y)
+int mul(int x, int y);
 void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_diagonal(int n);
+void print_square(int size);
